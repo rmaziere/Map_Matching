af81ee51bef332b6a2975c87e86aafd0786068fd
Rudolf MILLET <rudolf@masina.ensg.eu>
2016-11-08 11:08:31 +0100

    Mise en ligne >TG-46 #done

---
+++ MMModule/track.cpp
@@ -0,0 +1,10 @@
+#include "track.h"
+
+Track::Track(){
+
+
+}
+
+int main(){
+
+}
---
+++ MMModule/track.h
@@ -0,0 +1,20 @@
+#ifndef TRACK_H
+#define TRACK_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Track{
+
+    public:
+
+        Track();
+        ~Track();
+
+};
+
+#endif // TRACK_H
