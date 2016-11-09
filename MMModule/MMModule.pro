c22b2a24b1e91847c8ae22e9b4eb2c3771eaf310
Loic Messal <loic.messal@orange.fr>
2016-11-09 12:24:51 +0100

    Création de routes dans un réseau à partir d'un fichier csv TG-51 #ready-for-test

--- MMModule/MMModule.pro
+++ MMModule/MMModule.pro
@@ -4,7 +4,7 @@ QT += gui
 CONFIG += c++11
 
 TARGET = sandbox
-CONFIG += console
+CONFIG -= console
 CONFIG -= app_bundle
 
 TEMPLATE = app
--- MMModule/road.h
+++ MMModule/road.h
@@ -10,16 +10,22 @@ using namespace std;
 class Road {
 public:
     Road(string description){} // TODO
+    Road(vector<vector<double> > listOfCoordinates, long edgeId, long fromNodeId, long toNodeId); // TODO
+    Road(vector<Point> listOfPoints);
+
+    virtual ~Road();
+
 
     bool addPoint(double x, double y){ return true;} // TODO
     bool addPoint(long id, double x, double y){ return true;} // TODO
+    bool addPoint(Point){ return true;} // TODO
 
     Point *findPoint(long id){return NULL;} // TODO
 
 protected:
-    long id;
-    long fromNodeId;
-    long toNodeId;
+    long m_edgeId;
+    long m_fromNodeId;
+    long m_toNodeId;
     bool twoWay;
     float avgSpeed;
     int noOfPoints;
