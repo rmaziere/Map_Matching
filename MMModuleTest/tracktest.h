0a3b5119e4deada3408d19adb3f03d72c86c3a67
Rudolf MILLET <rudolf@masina.ensg.eu>
2016-11-10 09:43:22 +0100

    Mise en ligne >TG-31 #done

--- MMModuleTest/tracktest.h
+++ MMModuleTest/tracktest.h
@@ -22,11 +22,9 @@ TEST_F(TrackTest, Constructeurs)
 
     Track Trace1 = Track();
     EXPECT_EQ(0, Trace1.getPoints().size());
-
-    //Trace1.~Track()
 }
 
-TEST_F(TrackTest, Ajouter_un_point)
+TEST_F(TrackTest, AddPoint)
 {
 
     Track Trace3 = Track();
@@ -38,11 +36,9 @@ TEST_F(TrackTest, Ajouter_un_point)
     EXPECT_EQ(125, Trace3.getPoints()[0]->getAltitude());
     EXPECT_EQ(timeStamp, Trace3.getPoints()[0]->getTimeStamp());
     EXPECT_EQ(1, Trace3.getPoints().size());
-
-    //Trace3.~Track();
 }
 
-TEST_F(TrackTest, Suppression)
+TEST_F(TrackTest, DeletePoint)
 {
 
     Track Trace2 = Track();
@@ -61,18 +57,17 @@ TEST_F(TrackTest, Suppression)
     EXPECT_EQ(6, Trace2.getPoints()[5]->getLongitude());
     EXPECT_EQ(6, Trace2.getPoints()[5]->getAltitude());
     EXPECT_EQ(9, Trace2.getPoints().size());
-
-    //Trace2.~Track();
 }
+
 TEST_F(TrackTest, readFromCSV)
 {
 
-    Track Trace3 = Track();
-    Trace3.readFromCSV("../Data/Unit_tests_data_set/readFromCsvExemple.csv");
-    EXPECT_DOUBLE_EQ(1686462.894975865026936, Trace3.m_points[0]->m_x);
-    EXPECT_DOUBLE_EQ(246133.881616829748964, Trace3.m_points[0]->m_y);
-    EXPECT_FLOAT_EQ(47.6675, Trace3.m_points[1]->getLatitude());
-    EXPECT_FLOAT_EQ(0, Trace3.m_points[1]->getAltitude());
+    Track Trace4 = Track();
+    Trace4.readFromCSV("../Data/Unit_tests_data_set/readFromCsvExemple.csv");
+    EXPECT_DOUBLE_EQ(1686462.894975865026936, Trace4.getPoints()[0]->m_x);
+    EXPECT_DOUBLE_EQ(246133.881616829748964, Trace4.getPoints()[0]->m_y);
+    EXPECT_FLOAT_EQ(47.6675, Trace4.getPoints()[1]->getLatitude());
+    EXPECT_FLOAT_EQ(0, Trace4.getPoints()[1]->getAltitude());
 }
 
 #endif // TRACKTEST_H
